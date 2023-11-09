230c44e9579366b774a10666dcbd6b489e5a03be
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-09 15:49:36 +0200

    add greet view

--- starter/hello/views.py
+++ starter/hello/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World')
+
+
+def greet(request, name):
+    return HttpResponse(f'Hello, {name}')
