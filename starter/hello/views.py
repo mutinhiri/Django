50745ca407a44e7622e3655f73d0456691f74bb0
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-09 16:45:01 +0200

    ADd message render

--- starter/hello/views.py
+++ starter/hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request,'hello/index.html')
 
 
 def greet(request, name):
