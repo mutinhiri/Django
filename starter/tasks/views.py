84ddde2c91ab0a7191a014bdca1244dce0280db2
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-14 11:54:33 +0200

    Add new form from form field

--- starter/tasks/views.py
+++ starter/tasks/views.py
@@ -15,4 +15,7 @@ def index(request):
 
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    # return render(request, 'tasks/add.html')
+    return render(request, 'tasks/add.html', {
+        "form": NewTaskForm()
+    })
