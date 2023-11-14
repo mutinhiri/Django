cd2a00bf04702dac9b3e392873064296f2fb3a91
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-14 13:10:53 +0200

    Add new file

--- starter/tasks/views.py
+++ starter/tasks/views.py
@@ -23,6 +23,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data['task']
             tasks.append(task)
+            return HttpResponseRedirect()
         else:
             return render(request, 'tasks/add.html', {
                 "form": form
