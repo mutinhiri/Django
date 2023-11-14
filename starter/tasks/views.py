9523a416e379a190388c14ddccce252063f157d6
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-14 13:22:10 +0200

    Add new route method

--- starter/tasks/views.py
+++ starter/tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django import forms
+from django.urls import 
 
 tasks = ['foo', 'bar', 'baz']
 
@@ -23,7 +25,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data['task']
             tasks.append(task)
-            return HttpResponseRedirect(reverse('tasks'))
+            return HttpResponseRedirect(reverse('tasks:index'))
         else:
             return render(request, 'tasks/add.html', {
                 "form": form
