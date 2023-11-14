569f61f6ef99e8c117fd86116e2d914c12edf796
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-11-14 13:19:23 +0200

    Add new route

--- starter/tasks/views.py
+++ starter/tasks/views.py
@@ -23,7 +23,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data['task']
             tasks.append(task)
-            return HttpResponseRedirect()
+            return HttpResponseRedirect('/tasks')
         else:
             return render(request, 'tasks/add.html', {
                 "form": form
